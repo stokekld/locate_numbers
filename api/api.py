03b211ca3de7bb41e15fcf75715ef0fe34c94f75
Jesus Flores Acuña <joywarmgun@gmail.com>
2019-12-11 21:47:27 -0600

    sending file

--- api/api.py
+++ api/api.py
@@ -1,21 +1,46 @@
-from flask import Flask, request
-import csv
+from flask import Flask, request, send_file
+from phonenumbers import parse, is_valid_number, geocoder
+import csv, phonenumbers
 
 app = Flask(__name__)
 
 @app.route('/locate_numbers', methods = ['POST'])
 def hello_world():
 
-    inputFile = request.files["numbers"]
-    inputFile.save("input.csv")
+    if not "numbers" in request.files:
+        return "No file", 400
 
-    with open("input.csv") as csv_file:
-        csv_reader = csv.reader(csv_file)
+    request.files["numbers"].save("input.csv")
+
+    # creating file
+    with open('output.csv', mode='w') as csv_outputFile:
+        csv_outputFile.write("numbers,valid,location\n")
+    
+    with open("input.csv") as csv_inputFile:
+        csv_reader = csv.reader(csv_inputFile)
 
         for row in csv_reader:
-            print(row[0])
+            try:
+                number = parse(row[0], "US")
 
-    return 'Hello, World!'
+                if is_valid_number(number):
+                    valid = 'true'
+                else:
+                    valid = 'false'
+
+                if geocoder.description_for_number(number, "en") == '':
+                    where = 'n/a'
+                else:
+                    where = geocoder.description_for_number(number, "en")
+
+                with open('output.csv', mode='a') as csv_outputFile:
+                    csv_writer = csv.writer(csv_outputFile, delimiter=',')
+                    csv_writer.writerow([row[0], valid, where])
+            except:
+                print("Bad number")
+                pass
+
+    return send_file("output.csv", as_attachment=True)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9000, debug=True)
